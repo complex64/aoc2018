29b07496274bae3e31eda6bc251ee91188c9b549
Joe <joe@complex64.com>
2018-12-03 13:08:50 +0100

    second day

---
+++ cmd/2_inventory_management_system/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/complex64/aoc2018/internal/pkg/aoc"
+)
+
+// https://adventofcode.com/2018/day/2
+func main() {
+	wordsWithDouble, wordsWithTriple := 0, 0
+	err := aoc.CallForEachLineInFile("./input", func(line string) error {
+		d, t := GetDoubleAndTripleCount(line)
+		wordsWithDouble += d
+		wordsWithTriple += t
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(wordsWithDouble * wordsWithTriple)
+}
+
+func GetDoubleAndTripleCount(line string) (double int, triple int) {
+	// map of (b in [a..z]) -> count(b) in line
+	m := make(map[byte]int, 26)
+	for i := 0; i < len(line); i++ {
+		m[line[i]]++
+	}
+	for _, n := range m {
+		if n == 2 && double == 0 { // doubles count once
+			double++
+		} else if n == 3 && triple == 0 { // triples count once
+			triple++
+		}
+	}
+	return double, triple
+}
---
+++ cmd/2_inventory_management_system/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+// For example, if you see the following box IDs:
+// - abcdef contains no letters that appear exactly two or three times.
+// - bababc contains two a and three b, so it counts for both.
+// - abbcde contains two b, but no letter appears exactly three times.
+// - abcccd contains three c, but no letter appears exactly two times.
+// - aabcdd contains two a and two d, but it only counts once.
+// - abcdee contains two e.
+// - ababab contains three a and three b, but it only counts once.
+func TestGetDoubleAndTripleCount(t *testing.T) {
+	assertDoubleAndTriple(t, "abcdef", 0, 0)
+	assertDoubleAndTriple(t, "bababc", 1, 1)
+	assertDoubleAndTriple(t, "abbcde", 1, 0)
+	assertDoubleAndTriple(t, "abcccd", 0, 1)
+	assertDoubleAndTriple(t, "aabcdd", 1, 0)
+	assertDoubleAndTriple(t, "abcdee", 1, 0)
+	assertDoubleAndTriple(t, "ababab", 0, 1)
+}
+
+func assertDoubleAndTriple(t *testing.T, line string, double, triple int) {
+	d, tr := GetDoubleAndTripleCount(line)
+	assert.Equal(t, double, d)
+	assert.Equal(t, triple, tr)
+}
